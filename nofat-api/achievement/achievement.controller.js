fe80868a1b1c397b41d7a33b0173270f9e13ec47
Pablo Sanchez <pablo.sanchez@flagbit.de>
2018-12-31 18:51:24 +0100

    Saving achievement successfuly

--- nofat-api/achievement/achievement.controller.js
+++ nofat-api/achievement/achievement.controller.js
@@ -8,7 +8,7 @@ router.post('/', addAchievement);
 module.exports = router;
 
 function addAchievement(req, res, next) {
-    achievementService.add(req.params)
+    achievementService.add(req.body)
         .then(achievement => achievement ? res.json(achievement) : res.status(400).json({ message: 'Hey you bitch!' }))
         .catch(err => next(err));
 }
--- nofat-api/achievement/achievement.model.js
+++ nofat-api/achievement/achievement.model.js
@@ -3,11 +3,11 @@ const Schema = mongoose.Schema;
 
 const schema = new Schema({
     id: { type: Number, required: true },
-    workout_id: { type: Number, required: true },
-    user_id: { type: Number, required: true },
-    achievement: { type: Text, required: false },
-    photo_url: { type: Text, required: true },
-    video_url: { type: Text, required: true }
+    workoutId: { type: Number, required: true },
+    userId: { type: Number, required: true },
+    description: { type: String, required: false },
+    photoUrl: { type: String, required: true },
+    videoUrl: { type: String, required: true }
 });
 
 schema.set('toJSON', { virtuals: true });
