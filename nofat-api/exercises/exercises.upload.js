06e8216d8a60e3e8c5a7f878a9deb5ade766038e
Pablo Sanchez <pablo.sanchez@flagbit.de>
2019-02-11 09:07:56 +0100

    convert to hex

--- nofat-api/exercises/exercises.upload.js
+++ nofat-api/exercises/exercises.upload.js
@@ -9,28 +9,27 @@ exports.post = function (req, res) {
     var exercisesFile = req.files.file;
  
     var exercises = [];
-         
-    csv
-     .fromString(exercisesFile.data.toString(), {
-         headers: true,
-         ignoreEmpty: true
-     })
-     .on("data", function(data){
-
-
-
-        // TODO me he quedado aqui
-
-
-         // data['_id'] = new mongoose.Types.ObjectId();
-        data['_id'] = data.id;
-        exercises.push(data);
-     })
-     .on("end", function(){
-        Exercises.create(exercises, function(err, documents) {
-            if (err) throw err;
-         });
+        
+    console.log(convertToHex('12'));
+    // csv
+    //  .fromString(exercisesFile.data.toString(), {
+    //      headers: true,
+    //      ignoreEmpty: true
+    //  })
+    //  .on("data", function(data){
+    //     data['_id'] = new mongoose.Types.ObjectId(convertToHex(data.id));
+    //     exercises.push(data);
+    //  })
+    //  .on("end", function(){
+    //     Exercises.create(exercises, function(err, documents) {
+    //         if (err) throw err;
+    //      });
           
-         res.send(exercises.length + ' exercises have been successfully uploaded.');
-     });
+    //      res.send(exercises.length + ' exercises have been successfully uploaded.');
+    //  });
 };
+
+convertToHex(id) {
+    var hexBase = '0000000000000000';
+    
+}
